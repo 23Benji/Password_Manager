89acace5bba0450b947574eaad7ea0cd014124bb
23Benji <167515023+23Benji@users.noreply.github.com>
2024-09-11 18:00:00 +0200

    a

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <ctype.h>
+#include <ANSI-color-codes.h>
 
 struct Credential {
     char website[50];  // Website name
@@ -12,8 +13,11 @@ int add(void);
 int lookup(void);
 int edit(void);
 int delete(void);
+int printTitle(void);
 
+// Global variable for storing credentials
 struct Credential cred;
+#define RED "\e[0;31m"
 FILE *PassFile;
 
 
@@ -32,14 +36,17 @@ int main(void){
         }
     }
     
-    printf("Hello and Welcome to the Password Manager\n");
+    printTitle(); // Print title
+
+
 
 
   char choice;
 
     do {
-        printf("Options: (A)dd Password, (L)ookup Password, (E)dit Password, (D)elete Password, (Q)uit\n");
-        scanf(" %c", &choice);  // Notice the space before %c to ignore previous newline
+        printf(RED"Options: (A)dd Password, (L)ookup Password, (E)dit Password, (D)elete Password, (Q)uit\n");
+        printf(">> ");
+        scanf(" %c", &choice);  //Scan choice and remove space before %c to ignore previous newline
         choice = tolower(choice);  // Convert to lowercase
     } while (choice != 'a' && choice != 'l' && choice != 'e' && choice != 'd' && choice != 'q');
 
@@ -58,6 +65,8 @@ int main(void){
             break;
         case 'q':
             printf("Thank you for using the Password Manager!\n");
+            printf("See You soon! :D\n");
+
             break;
         default:
             printf("Invalid choice. Please try again.\n");
@@ -66,6 +75,18 @@ int main(void){
     fclose(PassFile);
     
     return 0;
+}
+
+int printTitle(void){
+
+    printf("      _____                                    _   __  __                                  \n");
+    printf("     |  __ \\                                  | | |  \\/  |                                 \n");
+    printf("     | |__) |_ _ ___ _____      _____  _ __ __| | | \\  / | __ _ _ __   __ _  __ _  ___ _ __ \n");
+    printf("     |  ___/ _` / __/ __\\ \\ /\\ / / _ \\| '__/ _` | | |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '__|\n");
+    printf("     | |  | (_| \\__ \\__ \\\\ V  V / (_) | | | (_| | | |  | | (_| | | | | (_| | (_| |  __/ |   \n");
+    printf("     |_|   \\__,_|___/___/ \\_/\\_/ \\___/|_|  \\__,_| |_|  |_|\___,_|_| |_|\\__,_|\\__,_|\\___|_|   \n");
+    printf("                                                                             __/ |          \n");
+    printf("                                                                            |___/           \n");
 }
 
 int add(void){
